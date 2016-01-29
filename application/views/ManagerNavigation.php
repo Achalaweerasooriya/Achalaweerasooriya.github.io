06e209b96393955b74dae2269e49fdf3fd8186a0
Achala <achala1994@gmail.com>
2016-01-29 11:26:24 +0530

    GUIs for dashboards

---
+++ application/views/ManagerNavigation.php
@@ -0,0 +1,43 @@
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="PM.html"><i class="fa fa-fw fa-dashboard"></i> Home</a>
+                    </li>
+                    <li>
+                        <a href="projects.html"><i class="fa fa-fw fa-bar-chart-o"></i> Projects</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-table"></i> Schedule</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-edit"></i>Reports</a></li>
+                
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-wrench"></i> Calander</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>Company Profile</a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="#">Dropdown Item</a>
+                            </li>
+                            <li>
+                                <a href="#">Dropdown Item</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="Create project.html"><i class="fa fa-fw fa-bar-chart-o"></i> Create Project</a>
+                    </li>
+                    <li>
+                        <a href="Create team.html"><i class="fa fa-fw fa-table"></i> Create Team</a>
+                    </li>
+               
+                </ul>
+                
+                
+            </div>
+            
+            <!-- /.navbar-collapse -->
+        </nav>
---
+++ application/views/PMNavigation.php
@@ -0,0 +1,43 @@
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="PM.html"><i class="fa fa-fw fa-dashboard"></i> Home</a>
+                    </li>
+                    <li>
+                        <a href="projects.html"><i class="fa fa-fw fa-bar-chart-o"></i> On-Going Projects</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-table"></i> Projects BackLog</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-edit"></i>Developer Teams</a></li>
+                
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-wrench"></i>Add New Project</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>Project Profile</a>
+                        <ul id="demo" class="collapse">
+                            <li>
+                                <a href="#">Client Details</a>
+                            </li>
+                            <li>
+                                <a href="#">View Project</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="Create project.html"><i class="fa fa-fw fa-bar-chart-o"></i>Create Team </a>
+                    </li>
+                    <li>
+                        <a href="Create team.html"><i class="fa fa-fw fa-table"></i> Profile</a>
+                    </li>
+               
+                </ul>
+                
+                
+            </div>
+            
+            <!-- /.navbar-collapse -->
+        </nav>
